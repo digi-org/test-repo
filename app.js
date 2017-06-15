a58cccd9b1e733d807438dd6f507ac30f65ff27f
Yogaraj Jawahar <Yogaraj.Jawahar@aig.com>
2017-06-15 15:54:11 -0400

    pagenotfound

--- app.js
+++ app.js
@@ -33,6 +33,7 @@
   app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
+    res.render('pagenotfound', { title: 'pagenotfound' });
     next(err);
   });
 
